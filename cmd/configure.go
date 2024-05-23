1774b3551a9e283e2c397ff02cd401b070e272dc
Nathan Kulewoshie <kulewoshienathan@gmail.com>
2024-05-23 18:38:12 +0000

    feat:lock: configuring credentials to jenkins server

---
+++ cmd/configure.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var configureCmd = &cobra.Command{
+	Use:   "configure",
+	Short: "Set up credentials to use jenkins server",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		// directory for credentials
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			if err := os.Mkdir(path, 0750); err != nil {
+				log.Fatalf("Error creating dir, %v", err)
+			}
+		}
+		// writing credentials
+		filePath := fmt.Sprintf("%s/config.yml", path)
+		data := fmt.Sprintf("credentials:\n\turl: %s\n\tusername: %s\n\tapiToken: %s\n", jenkinsURL, username, apiToken)
+		// data := fmt.Sprintf("url=%s\nusername=%s\napiToken=%s\n", jenkinsURL, username, apiToken) // required data
+		if err := os.WriteFile(filePath, []byte(data), 0644); err != nil {
+			log.Fatalf("Configuration failed, %v\n", err)
+		}
+
+		fmt.Println("Crendentials configured successfully")
+	},
+}
+
+func init() {
+	// configuration command, command flags
+	rootCmd.AddCommand(configureCmd)
+	configureCmd.Flags().StringVarP(&jenkinsURL, "url", "u", "", "jenkins URL")
+	configureCmd.Flags().StringVarP(&username, "username", "n", "", "jenkins username")
+	configureCmd.Flags().StringVarP(&apiToken, "token", "t", "", "jenkins api token")
+
+	// required fields
+	_ = configureCmd.MarkFlagRequired("url")
+	_ = configureCmd.MarkFlagRequired("username")
+	_ = configureCmd.MarkFlagRequired("token")
+}
